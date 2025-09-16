48aa25f2e1bae80cf285b265ad3c7f9860498294
Sopuruoti641 <sopuruoti641@gmail.com>
2025-09-16 03:09:09 -0700

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/news-explorer/">
+    <BrowserRouter
+      basename={import.meta.env.MODE === "production" ? "/news-explorer" : "/"}
+    >
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// "dev": "vite",
-// "build": "vite build",
-// "lint": "eslint .",
-// "preview": "vite preview",
-// "predeploy": "npm run build",
-// "deploy": "gh-pages -d dist"
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/news-explorer/",
+//   plugins: [react()],
+//   server: {
+//     port: 3001,
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/news-explorer/",
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? "/news-explorer/" : "/",
   server: {
     port: 3001,
   },
-});
+}));
