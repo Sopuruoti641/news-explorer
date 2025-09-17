2deefaae44ce2253cae0775df3bc8b02600e8496
Sopuruoti641 <sopuruoti641@gmail.com>
2025-09-16 20:49:48 -0700

    changes

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,9 +1,29 @@
-const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+// const apiKey = "28d60a76841b40e39864ccfa7129de78";
 
-const baseUrl =
-  import.meta.env.MODE === "production"
-    ? "https://nomoreparties.co/news/v2/everything"
-    : "https://newsapi.org/v2/everything";
+// const baseUrl =
+//   import.meta.env.MODE === "production"
+//     ? "https://nomoreparties.co/news/v2/everything"
+//     : "https://newsapi.org/v2/everything";
+
+// export function fetchNewsArticles(query) {
+//   const sevenDaysAgo = new Date();
+//   sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+//   const from = sevenDaysAgo.toISOString().split("T")[0];
+//   const to = new Date().toISOString().split("T")[0];
+
+//   return fetch(
+//     `${baseUrl}?q=${encodeURIComponent(
+//       query
+//     )}&from=${from}&to=${to}&pageSize=100&apiKey=${apiKey}`
+//   ).then((res) => {
+//     if (!res.ok) throw new Error("Failed to fetch news");
+//     return res.json();
+//   });
+// }
+
+const apiKey = "28d60a76841b40e39864ccfa7129de78";
+const baseUrl = "https://nomoreparties.co/news/v2/everything";
 
 export function fetchNewsArticles(query) {
   const sevenDaysAgo = new Date();
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/news-explorer/",
-//   plugins: [react()],
-//   server: {
-//     port: 3001,
-//   },
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
